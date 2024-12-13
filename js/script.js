49d7a51cfbd6b9f2adf2d7b3751ce61fd34850ce
ZeusMoreira <zeus.moreira111@gmail.com>
2024-12-13 05:14:17 -0400

    Adicionando novos 2 projetos para o portofolio e arrumando threshold para atualização correta da url

--- js/script.js
+++ js/script.js
@@ -138,7 +138,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const sections = document.querySelectorAll('section');
     const options = {
         root: null,
-        threshold: 1 
+        threshold: 0.5 
     };
 
     const observer = new IntersectionObserver((entries, observer) => {
